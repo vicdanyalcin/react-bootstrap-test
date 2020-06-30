796407fc0c1a38727925f069f484cbaf59f6beeb
vicdanyalcin <vicdanyalcin.vyn@gmail.com>
2020-06-30 14:57:41 +0300

    Updates

---
+++ precache-manifest.48c3375132b966bb844d9f92e2661d58.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3139082cd3e7ff009ddca4b258a6deb5",
+    "url": "/vicdanyalcin/react-bootstrap-test/index.html"
+  },
+  {
+    "revision": "11099dee5997432bb11c",
+    "url": "/vicdanyalcin/react-bootstrap-test/static/css/main.c644cdcc.chunk.css"
+  },
+  {
+    "revision": "04d75060c25db1c09b00",
+    "url": "/vicdanyalcin/react-bootstrap-test/static/js/2.d05f63a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vicdanyalcin/react-bootstrap-test/static/js/2.d05f63a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11099dee5997432bb11c",
+    "url": "/vicdanyalcin/react-bootstrap-test/static/js/main.e2401511.chunk.js"
+  },
+  {
+    "revision": "ab994481e2d6e773d7ae",
+    "url": "/vicdanyalcin/react-bootstrap-test/static/js/runtime-main.5b83461d.js"
+  },
+  {
+    "revision": "d3b4a514dfb172f6ac1817f5bdb974ad",
+    "url": "/vicdanyalcin/react-bootstrap-test/static/media/bg-masthead.d3b4a514.jpg"
+  },
+  {
+    "revision": "5ac05903cff7fd9e6034b0f9539da51d",
+    "url": "/vicdanyalcin/react-bootstrap-test/static/media/bg-signup.5ac05903.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vicdanyalcin/react-bootstrap-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
